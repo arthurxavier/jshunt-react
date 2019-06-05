a88b97a333e852c0575acfe5468af3813cb7a9d8
ArthurXG <arthurxaviergiffoni@gmail.com>
2019-06-04 22:18:17 -0300

    Request api using axios

--- src/App.js
+++ src/App.js
@@ -3,10 +3,11 @@ import React, { Component } from "react";
 import './stylesDefault.css'
 
 import Header from './components/Header/index';
-
+import Main from './pages/main';
 const App = () =>(
   <div className="App">
     <Header />
+    <Main />
   </div>
 );
 
